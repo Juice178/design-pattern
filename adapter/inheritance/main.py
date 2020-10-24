2200e63fab2845de2a8d28ecbb62f8dd066782ba
Kenta <kentasuzuki5555@gmail.com>
2020-10-24 16:00:20 +0900

    Add an assertion to check if an instace is a target

--- adapter/inheritance/main.py
+++ adapter/inheritance/main.py
@@ -1,7 +1,9 @@
 from print_banner import PrintBanner 
+from printing import Printing
 
 def main():
     p = PrintBanner("Hello")
+    assert isinstance(p, Printing)
     p.print_weak()
     p.print_strong()
 
