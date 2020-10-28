25f4e7eaa625c03f275dd92a5a16756a429f5518
Kenta <kentasuzuki5555@gmail.com>
2020-10-28 23:32:00 +0900

    Add assertions to check if an instance is a Product

--- prototype/main.py
+++ prototype/main.py
@@ -1,4 +1,5 @@
 from framework import Manager
+from framework import Product
 from message_box import MessageBox
 from underline_pen import UnderlinePen
 from copy import copy
@@ -13,10 +14,13 @@ def main():
     manager.register("slash box", sbox)
 
     p1 = manager.create("strong message")
+    assert isinstance(p1, Product)
     p1.use("Hello, world.")
     p2 = manager.create("warning box")
+    assert isinstance(p2, Product)
     p2.use("Hello, world.")
     p3 = manager.create("slash box")
+    assert isinstance(p3, Product)
     p3.use("Hello, world.")
 
 if __name__ == "__main__":
