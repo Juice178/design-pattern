649f88101fe9630c6bb62a55a496b9c966e285d9
Kenta <kentasuzuki5555@gmail.com>
2021-01-01 12:35:44 +0900

    Delete unnecessary comments

--- src/visitor/list_visitor.py
+++ src/visitor/list_visitor.py
@@ -17,14 +17,15 @@ class ListVisitor(Visitor):
 
     @visit.register
     def _(self, file: File) -> None:
-        print("file")
+        #print("file")
         print(f"{self._current_dir}/{file}")
 
     @visit.register
     def _(self, directory: Directory) -> None:
         #print("dir")
+        print(f"{self._current_dir}/{directory}")
         save_dir = self._current_dir
-        #print(f"save_dir: {save_dir}")
+        # print(f"save_dir: {save_dir}")
         ListVisitor._current_dir = f"{self._current_dir}/{directory.get_name()}"
         # print(f"_current_dir: {self._current_dir}")
         it = directory.iterator()
