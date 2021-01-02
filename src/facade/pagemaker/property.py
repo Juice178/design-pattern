427154badf93496e2b7172d40e7f8df2e6546655
Kenta <kentasuzuki5555@gmail.com>
2021-01-02 17:26:13 +0900

    Delete a unused file

--- src/facade/pagemaker/property.py
+++
@@ -1,8 +0,0 @@
-import configparser
-
-
-class Property:
-    def __init__(self) -> None:
-        self.config = configparser.ConfigParser()
-    def load(self, filename: str) -> configparser.ConfigParser:
-        return self.config.read(filename)
